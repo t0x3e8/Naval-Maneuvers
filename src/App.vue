8878a0083f4fed14896f1efee660907af2dcfda6
Jarek <t0x3e8@gmail.com>
2023-11-12 17:51:29 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  #app {
+    text-align: center;
+  }
+</style>
