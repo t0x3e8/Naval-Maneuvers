2fbddf7dab7b20ba627c46fc689e98f1913112f5
Jarek <t0x3e8@gmail.com>
2023-11-27 21:18:46 +0100

    Utils unitests added

--- src/client/GameEngine/utils.js
+++ src/client/GameEngine/utils.js
@@ -1,8 +1,11 @@
 class Utils {
+  /**
+   * Generates a random integer from 0 up to (but not including) a given max.
+   * @param {number} max - Upper limit for random number, floored if not an integer.
+   * @returns {number} A random integer.
+   */
   static getRandom (max) {
-    const max1 = Math.floor(max)
-
-    return Math.floor(Math.random() * max1)
+    return Math.floor(Math.random() * Math.floor(max))
   }
 }
 
